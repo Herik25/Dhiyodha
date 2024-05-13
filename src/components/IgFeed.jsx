cf3916621aba0480de23446b372b8d90d3cfa79f
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-05-13 13:55:17 +0530

    update className in IgFeed

--- src/components/IgFeed.jsx
+++ src/components/IgFeed.jsx
@@ -4,7 +4,7 @@ function IgFeed() {
   return (
     <div>
       <div id="curator-feed-default-feed-layout">
-        <a href="https://curator.io" target="_blank" class="crt-logo crt-tag">
+        <a href="https://curator.io" target="_blank" className="crt-logo crt-tag">
           Powered by Curator.io
         </a>
       </div>
