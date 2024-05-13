01436104cbf52cb8dec3e63a449a5bfc42061d84
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-05-13 14:03:58 +0530

    Update the igfeed section

--- src/components/IgFeed.jsx
+++ src/components/IgFeed.jsx
@@ -3,11 +3,7 @@ import React from "react";
 function IgFeed() {
   return (
     <div>
-      <div id="curator-feed-default-feed-layout">
-        <a href="https://curator.io" target="_blank" className="crt-logo crt-tag">
-          Powered by Curator.io
-        </a>
-      </div>
+      <div id="curator-feed-default-feed-layout"></div>
     </div>
   );
 }
